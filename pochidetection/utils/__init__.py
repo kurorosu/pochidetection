16e17bdeb2bd3aacc0e8e3a084884e8513c0d92b
kurorosu <149222114+kurorosu@users.noreply.github.com>
2026-01-24 14:51:24 +0900

    feat: 作業ディレクトリ自動生成機能を追加

--- pochidetection/utils/__init__.py
+++ pochidetection/utils/__init__.py
@@ -1,5 +1,6 @@
 """ユーティリティモジュール."""
 
 from pochidetection.utils.config_loader import ConfigLoader
+from pochidetection.utils.work_dir import WorkspaceManager
 
-__all__ = ["ConfigLoader"]
+__all__ = ["ConfigLoader", "WorkspaceManager"]
